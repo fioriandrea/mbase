fdbe336a2510d2338e2a2eb3301ead4d971499e4
Andrea Fiori <andrea.fiori.1998@gmail.com>
2021-07-23 12:07:11 +0200

    finisci output su file

--- mbase.py
+++ mbase.py
@@ -72,27 +72,29 @@ def output(bset):
     pqueue.put(bset)
 
 def set_to_string(s):
-    global symbol_mapping
-    if symbol_mapping == None:
-        return str(s)
-    buffer = []
-    for elem in s:
-        buffer.append(symbol_mapping[elem])
-    return '{' + ','.join(buffer) + '}'
+    return str(s)
+
+def write_header(f, matrix_name, matrix, optimize):
+    print('Matrice: %s' % (matrix_name), file=f)
+    print('Numero insiemi: %s' % (len(matrix)), file=f)
+    print('Numero elementi dominio: %s' % (len(matrix[0])), file=f)
+    print('', file=f)
+    print('Inizio elaborazione (%s)' % ('con preprocessing' if optimize else 'senza preprocessing'), file=f)
+    print('', file=f)
+
+def write_trailer(f, count, min_cardinality, max_cardinality, execution_time):
+    print('Numero hitting set trovati: %d' % (count), file=f)
+    print('Cardinalità minima: %d' % (min_cardinality), file=f)
+    print('Cardinalità massima: %d' % (max_cardinality), file=f)
+    print('Tempo di esecuzione: %f secondi' % (execution_time), file=f)
+    print('--------------------------------', file=f)
 
 def file_writer(pqueue, matrix_name, matrix, optimize):
-    # TODO: da ripulire
     signal.signal(signal.SIGINT, signal.SIG_IGN)
-    filename = matrix_name + '.output.txt'
-    print(filename)
+    filename = matrix_name + '.output'
     with open(filename, 'w') as f:
-        print('Matrice: %s' % (matrix_name), file=f)
-        print('Numero insiemi: %s' % (len(matrix)), file=f)
-        print('Numero elementi dominio: %s' % (len(matrix[0])), file=f)
-        if optimize:
-            print('Inizio elaborazione (con preprocessing)', file=f)
-        else:
-            print('Inizio elaborazione (senza preprocessing)', file=f)
+        write_header(f, matrix_name, matrix, optimize)
+        write_header(sys.stdout, matrix_name, matrix, optimize)
         while True:
             s = pqueue.get()
             if s == 'COMPLETED':
@@ -102,55 +104,16 @@ def file_writer(pqueue, matrix_name, matrix, optimize):
         min_cardinality = pqueue.get()
         max_cardinality = pqueue.get()
         execution_time = pqueue.get()
-        print('Numero hitting set trovati: %d' % (count), file=f)
-        print('Cardinalità minima: %d' % (min_cardinality), file=f)
-        print('Cardinalità massima: %d' % (max_cardinality), file=f)
-        print('Tempo di esecuzione: %f secondi' % (execution_time), file=f)
+        print('', file=f)
+        write_trailer(f, count, min_cardinality, max_cardinality, execution_time)
+        write_trailer(sys.stdout, count, min_cardinality, max_cardinality, execution_time)
 
 # preprocessing
-
-def parse_mapping(line):
-    i = 0
-    def skip_spaces():
-        nonlocal line
-        nonlocal i
-        while i < len(line) and line[i] in (' ', '\n', '\t'):
-            i = i + 1
-
-    def skip_digits():
-        nonlocal line
-        nonlocal i
-        while i < len(line) and line[i] >= '0' and line[i] <= '9':
-            i = i + 1
-
-    def read_symbol():
-        nonlocal line
-        nonlocal i
-        buffer = []
-        while i < len(line) and line[i] != ')':
-            buffer.append(line[i])
-            i = i + 1
-        return ''.join(buffer)
-
-    global symbol_mapping
-    symbol_mapping = []
-    while i < len(line):
-        skip_spaces()
-        skip_digits()
-        i = i + 1 # salta parentesi (
-        symbol_mapping.append(read_symbol())
-        i = i + 1 # salta parentesi )
-        skip_spaces()
-    return symbol_mapping
 
 def parse_matrix_file(filename):
     matrix = []
     with open(filename, 'r') as f:
         for line in f:
-            if line.startswith(';;; Map'):
-                line = line[len(';;; Map'):]
-                parse_mapping(line)
-                continue
             if line.startswith(';;;') or len(line.strip()) == 0:
                 continue
             row = line.strip().split(' ')[:-1]
@@ -213,13 +176,12 @@ eps_max = None
 count = 0
 max_cardinality = None
 min_cardinality = None
-symbol_mapping = None
 matrix = None
 pqueue = None
 
 for arg in args:
     matrix = parse_matrix_file(arg)
-    # TODO da ripulire
+    previous_execution_time = None
     for optimize in (False, True):
         start_time = time.time()
         if len(matrix) == 0:
@@ -232,18 +194,20 @@ for arg in args:
         max_cardinality = 0
         set_list = from_matrix_to_set_list(matrix)
         pqueue = Queue()
-        # TODO da ripulire
-        matrix_name = os.path.basename(arg) + ("_optimized" if optimize else "")
+        matrix_name = os.path.basename(arg) + (".optimized" if optimize else "")
         writer = Process(target=file_writer, args=(pqueue, matrix_name, matrix, optimize))
         writer.start()
         try:
             mbase(set_list)
         except KeyboardInterrupt:
             pass
-        # TODO da ripulire
         pqueue.put('COMPLETED')
         pqueue.put(count)
         pqueue.put(min_cardinality)
         pqueue.put(max_cardinality)
-        pqueue.put(time.time() - start_time)
+        current_execution_time = time.time() - start_time
+        pqueue.put(current_execution_time)
         writer.join()
+        if optimize:
+            print('Guadagno in esecuzione per il preprocessing: %f secondi' % (previous_execution_time - current_execution_time))
+        previous_execution_time = current_execution_time
